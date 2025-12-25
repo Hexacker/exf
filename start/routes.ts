1a6f92a5e1907ba64dc2e9992d6b17bc5d2ee529
Abdelmadjid Cherfaoui <abdelmadjid.cherfaoui@gmail.com>
2025-12-25 04:36:06 -0500

    fix the routing

--- start/routes.ts
+++ start/routes.ts
@@ -14,7 +14,7 @@ const SignupController = () => import('#controllers/auth/signup_controller')
 const VerifyController = () => import('#controllers/auth/verify_controller')
 const SigninController = () => import('#controllers/auth/signin_controller')
 const SignoutController = () => import('#controllers/auth/signouts_controller')
-//const resetPasswordController = () => import('#controllers/auth/reset_password_controller')
+const resetPasswordController = () => import('#controllers/auth/reset_password_controller')
 
 router.on('/').render('pages/home')
 
@@ -27,23 +27,23 @@ router
     router.post('/signin', [SigninController, 'store']).as('signin.store').use(middleware.guest())
     router.post('signout', [SignoutController, 'handle']).as('signout').use(middleware.auth())
     router.on('/dashboard').render('pages/auth/welcome').as('dashboard').use(middleware.auth())
-    // router
-    //   .get('/reset', [resetPasswordController, 'view'])
-    //   .as('resetPassword.show')
-    //   .use(middleware.guest())
-    // router
-    //   .post('/reset', [resetPasswordController, 'store'])
-    //   .as('resetPassword.store')
-    //   .use(middleware.guest())
-    // router
-    //   .get('/password/:token', [resetPasswordController, 'resetView'])
-    //   .as('password.resetView')
-    //   .use(middleware.guest())
+    router
+      .get('/reset', [resetPasswordController, 'view'])
+      .as('resetPassword.show')
+      .use(middleware.guest())
+    router
+      .post('/reset', [resetPasswordController, 'store'])
+      .as('resetPassword.store')
+      .use(middleware.guest())
+    router
+      .get('/password/:token', [resetPasswordController, 'resetView'])
+      .as('password.resetView')
+      .use(middleware.guest())
 
-    // router
-    //   .post('/password/:token', [resetPasswordController, 'handle'])
-    //   .as('password')
-    //   .use(middleware.guest())
+    router
+      .post('/password/:token', [resetPasswordController, 'handle'])
+      .as('password')
+      .use(middleware.guest())
   })
   .prefix('/auth')
   .as('auth')
